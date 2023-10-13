f6d8b9f526e35790faa909464fb0b222acdd1e7a
2023284 <2023284@student.cct.ie>
2023-10-13 21:42:18 +0100

    Statement in OOCMiniHW2
    Add the statement in OOCMiniHW2
    according to the implementation requirements

--- OOCMiniHW2/src/oocminihw2/OOCMiniHW2.java
+++ OOCMiniHW2/src/oocminihw2/OOCMiniHW2.java
@@ -14,7 +14,20 @@ public class OOCMiniHW2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Aeroplane airplane = new Aeroplane(520, "Airbus", "Aircraft", 180);
+        Ship ship = new Ship(10, "Ship", "Cargo Ship", 2);
+        Automobile car = new Automobile(80, "Toyota ", "Mark X", 6);
+
+        ship.hoistSail();
+        ship.landHo();
+
+        airplane.changeAltitude(44000);
+        System.out.println("Current Altitude: " + airplane.getAltitude());
+
+        car.accelerate(26);
+        System.out.println("Current Speed: " + car.getSpeed());
+    }
         
     }
     
-}
+
